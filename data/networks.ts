1b38aa27e077ba6f24399717296dd0672c15c274
sixtusdeveloper <sixtusushrey@gmail.com>
2025-02-20 00:21:22 +0100

    updated the network data file

--- data/networks.ts
+++ data/networks.ts
@@ -1,23 +1,50 @@
+
 export const networks = [
     {
-      id: "mtn",
+      id: 1,
       name: "MTN",
-      plans: [100, 200, 500, 1000, 2000, 5000],
+      plans: ["N250 1GB - 30days", "N500 2GB - 30days", "N1000 5GB - 30days"],
     },
     {
-      id: "airtel",
-      name: "Airtel",
-      plans: [100, 200, 500, 1000, 2000, 5000],
-    },
-    {
-      id: "glo",
+      id: 2,
       name: "Glo",
-      plans: [100, 200, 500, 1000, 2000, 5000],
+      plans: ["N200 1.5GB - 30days", "N400 3GB - 30days", "N800 7GB - 30days"],
     },
     {
-      id: "9mobile",
+      id: 3,
+      name: "Airtel",
+      plans: ["N300 2GB - 30days", "N600 4.5GB - 30days", "N1200 10GB - 30days"],
+    },
+    {
+      id: 4,
       name: "9mobile",
-      plans: [100, 200, 500, 1000, 2000, 5000],
+      plans: ["N500 2GB - 30days", "N1000 5GB - 30days", "N2000 12GB - 30days"],
     },
   ];
-  
+
+
+
+
+
+//   export const networks = [
+//     {
+//       id: "mtn",
+//       name: "MTN",
+//       plans: [100, 200, 500, 1000, 2000, 5000],
+//     },
+//     {
+//       id: "airtel",
+//       name: "Airtel",
+//       plans: [100, 200, 500, 1000, 2000, 5000],
+//     },
+//     {
+//       id: "glo",
+//       name: "Glo",
+//       plans: [100, 200, 500, 1000, 2000, 5000],
+//     },
+//     {
+//       id: "9mobile",
+//       name: "9mobile",
+//       plans: [100, 200, 500, 1000, 2000, 5000],
+//     },
+//   ];
